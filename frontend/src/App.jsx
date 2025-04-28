7340509bfd29b3c89835510c619590d027c06d4d
Guevara <rakha.guevara2505@gmail.com>
2025-04-28 16:01:38 +0700

    "rakha commit kedua"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,81 +1,106 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import backgroundImg from "./Assets/bg.png";
 
 export default function App() {
+  const [showBanner, setShowBanner] = useState(true);
+
   return (
     <>
-      <nav>
-        <div className="left">
-          <p>SHOP.CO</p>
-
-          <ul>
-            <li>
-              <a href="">Shop</a>
-            </li>
-            <li>
-              <a href="">On sale</a>
-            </li>
-            <li>
-              <a href="">New Arrival</a>
-            </li>
-            <li>
-              <a href="">Brands</a>
-            </li>
-          </ul>
+      {/* banner */}
+      {showBanner && (
+        <div className="signup-banner">
+          <div className="con-left-signup-banner"></div>
+          <div className="con-signup-banner">
+            <span>Sign up and get 20% off to your first order.</span>
+            <a href="">Sign Up Now</a>
+          </div>
+          <button className="banner-close" onClick={() => setShowBanner(false)}>
+            &times;
+          </button>
         </div>
+      )}
 
-        <div className="right">
-          <div className="search-bar">
-            <Icon icon="ic:baseline-search" width="24" height="24" />
-            <input type="" placeholder="Search for product.." />
+      <nav>
+        <div className="wrapper-nav">
+          {/* nav left  start */}
+          <div className="nav left">
+            <p className="logo">SHOP.CO</p>
+            <div className="nav-links">
+              <ul>
+                <li>
+                  <a href="">Shop</a>
+                </li>
+                <li>
+                  <a href="">On Sale</a>
+                </li>
+                <li>
+                  <a href="">New Arrival</a>
+                </li>
+                <li>
+                  <a href="">Brands</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="auth-content">
-            <Icon
-              icon="majesticons:shopping-cart-line"
-              width="24"
-              height="24"
-            />
-            <Icon icon="gg:profile" width="24" height="24" />
+          {/* nav left end */}
+
+          {/* nav right start */}
+          <div className="nav-right">
+            <form action="" className="search-bar">
+              <div className="con-search-bar">
+                <Icon
+                  icon="bitcoin-icons:search-filled"
+                  width="30"
+                  height="30"
+                  style={{ color: "#848484" }}
+                />
+                <input
+                  type="text"
+                  placeholder="Search for product..."
+                  className="search-input"
+                />
+              </div>
+            </form>
+            <div className="auth-content">
+              <div className="cart">
+                <Icon icon="proicons:cart" width="24" height="24" />
+              </div>
+              <div className="user">
+                <Icon icon="qlementine-icons:user-16" width="24" height="24" />
+              </div>
+            </div>
           </div>
+          {/* nav right end */}
         </div>
       </nav>
 
       {/* body */}
-
-      <div className="container">
-        <img src={backgroundImg} alt="Background Image" />
-
-        <div className="content">
-          <div className="first-p">
-            {" "}
-            <p>FIND CLOTHES THAT MATCHES YOUR STYLE</p>
-          </div>
-
-          <p>
+      <div className="section-1">
+        <div className="left-section-1">
+          <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+          <p className="section-1-p">
             Browse through our diverse range of meticulously crafted garments,
-            designed to bring out your individuality and cater to your sense of
-            style.
+            designed to bring out your individualtiy and cater to your sense of
+            style
           </p>
-
-          <button className="button-shop">
+          <button>
             <a href="">Shop Now</a>
           </button>
-
-          {/*  rating */}
-          <div className="rating">
-            <div className="rating-item">
-              <p className="rating-number">200+</p>
-              <p className="rating-text">International Brands</p>
+          <div className="con-testi-section-1">
+            <div className="brands">
+              <p className="testi-section-1-title">200+</p>
             </div>
-            <div className="rating-item">
-              <p className="rating-number">2,000+</p>
-              <p className="rating-text">High Quality Products</p>
+            <div className="testi-separator"></div>
+            <div className="products">
+              <p className="testi-section-1-title">2.000+</p>
+              <p className="testi-section-1-desc">High-Quality Products</p>
             </div>
-            <div className="rating-item">
-              <p className="rating-number">30,000+</p>
-              <p className="rating-text">Happy Customers</p>
+            <div className="testi-separator"></div>
+            <div className="costumers">
+              <p className="testi-section-1-title">30,000+</p>
+              <p className="Happy Costumers"></p>
             </div>
           </div>
         </div>
